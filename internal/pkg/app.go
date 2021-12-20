08b954ade97182c7604c68b0450c8ba98d70200e
M1racle.Hao <crazyhl@163.com>
2021-12-20 15:52:14 +0800

    doc: 添加 swagger 示例

--- internal/pkg/app.go
+++ internal/pkg/app.go
@@ -2,12 +2,14 @@ package pkg
 
 import "github.com/gin-gonic/gin"
 
+// 自行封装的 app 实例，基础就是 gin 的 engine
 type app struct {
 	*gin.Engine
 }
 
 var appInstance *app
 
+// GetInstance 获取实例，单例模式
 func GetInstance() *app {
 	if appInstance == nil {
 		appInstance = &app{
--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	_ "github.com/cimple-admin/cimple-cms-backend/docs"
 	"github.com/cimple-admin/cimple-cms-backend/internal/pkg"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"net/http"
 )
 
+// @BasePath /
+
+// PingExample godoc
+// @Summary ping example
+// @Description do ping
+// @Accept json
+// @Produce json
+// @Success 200 {string} json "{"ping": 1}"
+// @Router /ping [get]
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"pong": 1,
+	})
+}
+
+// 整个项目的入口
 func main() {
 	app := pkg.GetInstance()
-
-	app.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"pong": 1,
-		})
-	})
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	app.GET("/ping", pong)
 
 	app.Run()
 }
