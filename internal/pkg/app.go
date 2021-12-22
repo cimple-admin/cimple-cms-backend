203b2eb83f48b6d29d912ec3edca6e55161991e5
M1racle.Hao <crazyhl@163.com>
2021-12-22 11:56:54 +0800

    refactor: 自行实现 Run 方法，这样可以通过 http.Server 设置一些参数

--- internal/pkg/app.go
+++ internal/pkg/app.go
@@ -1,16 +1,25 @@
 package pkg
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 // 自行封装的 app 实例，基础就是 gin 的 engine
 type app struct {
 	*gin.Engine
 }
 
+// AppInstance 对外暴露的 app 实例
 var AppInstance *app
 
 func init() {
 	AppInstance = &app{
 		gin.Default(),
 	}
+}
+
+func (app *app) Run() error {
+	server := &http.Server{Addr: ":8080", Handler: app}
+	return server.ListenAndServe()
 }
